8eb17ca55d296a9ed4dacff90bcac559e1ed6bff
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-04-09 16:56:15 -0400

    Updated code display method

--- movie.js
+++ movie.js
@@ -8,30 +8,32 @@ const movieData = await getData(
   `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_watch_monetization_types=flatrate&with_genres=99&with_keywords=9882`
 );
 
-console.log(movieData.results[0]);
+let x=0;
 
-document.getElementById("movieTitle").innerHTML = movieData.results[0].title;
+console.log(movieData.results[x]);
+
+document.getElementById("movieTitle").innerHTML = movieData.results[x].title;
 document.getElementById("movieOverview").innerHTML =
-  movieData.results[0].overview;
+  movieData.results[x].overview;
 document.getElementById("moviePopularity").innerHTML =
-  movieData.results[0].popularity;
+  movieData.results[x].popularity;
 document.getElementById("movieReleaseDate").innerHTML =
-  movieData.results[0].release_date;
-document.getElementById("movieId").innerHTML = movieData.results[0].id;
+  movieData.results[x].release_date;
+document.getElementById("movieId").innerHTML = movieData.results[x].id;
 document.getElementById("movieOriginalLanguages").innerHTML =
-  movieData.results[0].original_language;
-document.getElementById("movieVideo").innerHTML = movieData.results[0].video;
+  movieData.results[x].original_language;
+  document.getElementById("movieVideo").innerHTML = movieData.results[x].video;
 document.getElementById("movieVoteAverage").innerHTML =
-  movieData.results[0].vote_average;
+  movieData.results[x].vote_average;
 document.getElementById("movieVoteCount").innerHTML =
-  movieData.results[0].vote_count;
+  movieData.results[x].vote_count;
 
 const posterImg = document.createElement("img");
-posterImg.src = `https://image.tmdb.org/t/p/original/${movieData.results[0].poster_path}`;
+posterImg.src = `https://image.tmdb.org/t/p/original/${movieData.results[x].poster_path}`;
 document.body.appendChild(posterImg);
 posterImg.setAttribute("id", "poster-img");
 
 const backdropImg = document.createElement("img");
-backdropImg.src = `https://image.tmdb.org/t/p/original/${movieData.results[0].backdrop_path}`;
+backdropImg.src = `https://image.tmdb.org/t/p/original/${movieData.results[x].backdrop_path}`;
 document.body.appendChild(backdropImg);
 backdropImg.setAttribute('img', 'backdrop-img')
