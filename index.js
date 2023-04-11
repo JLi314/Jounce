3c4d010fcdde95775ba90e4c21506a281ed88f77
jiahua.li12321@gmail.com <Jiahua.li12321@gmail.com>
2023-04-11 19:33:03 -0400

    Increased observer treshhold

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const faders = document.querySelectorAll(".fade-in");
 // const sliders = document.querySelectorAll(".slide-in");
 
 const appearOptions = {
-  threshold: 0,
+  threshold: 0.25,
   rootMargin: "0px 0px -250px 0px",
 };
 
